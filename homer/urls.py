d88a28a2702fa44b8c7e1a2fa238795b2fea9a9c
magrinidiego <malkavian.rules@gmail.com>
2013-02-22 09:59:56 +0100

    Added the modified files for Homepage

--- homer/urls.py
+++ homer/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -20,3 +22,12 @@ urlpatterns = patterns('',
 urlpatterns += patterns('estates.views',
     url(r'^estate/', 'home', name='home'),
 )
+
+
+#For media files
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
--- homer/views.py
+++ homer/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from django.db import models
+from django.contrib.flatpages.models import FlatPage
 
 def home(request):
-    return render_to_response('home.html')
+    DEFAULT_ESTATES_HOME = "estates_home/"
+    DEFAULT_ADMINISTRATOR_HOME = "administration_home/"
+    DEFAULT_SUMMER_RENT_HOME = "summer_rent_home/"
+    
+    estates_flatpage = FlatPage.objects.get(url="/%s" % DEFAULT_ESTATES_HOME)
+    administrator_flatpage = FlatPage.objects.get(url="/%s" % DEFAULT_ADMINISTRATOR_HOME)
+    summer_rent_flatpage = FlatPage.objects.get(url="/%s" % DEFAULT_SUMMER_RENT_HOME)
+
+    
+    context = { 'estates_flatpage' : estates_flatpage, 
+               'administrator_flatpage' : administrator_flatpage,
+               'summer_rent_flatpage' : summer_rent_flatpage, }
+    return render_to_response('home.html', context_instance=RequestContext(request, context))
