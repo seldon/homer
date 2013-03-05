8c0e3a9456c4209cde85ef77db07fec28e2bb2ff
Diego Magrini <malkavian.rules@gmail.com>
2013-03-05 13:03:04 +0100

    Initial setup for Simply Block, some improvement to .gitignore file.

--- simplyblock/models.py
+++ simplyblock/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Content(models.Model):
+    title = models.CharField(max_length=300)
+    body = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    block_name = models.CharField(max_length=300,unique)
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()   
---
+++ simplyblock/templatetags/simplyblock_tags.py
@@ -0,0 +1,14 @@
+from django import template
+from simplyblock import Content
+class BlockContentNode(Node):
+    def render(self):
+        block_content = 
+        return ''
+
+
+
+
+def get_block_content(parser,token):
+    return BlockContentNode
+
+
